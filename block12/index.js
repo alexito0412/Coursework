ef4860bf90bfd86a7f77a64f915cfc3a022c9ef4
Alex Aguilar <alex.aguilar0412@gmail.com>
2023-09-26 22:27:10 -0700

    incr changes

---
+++ block12/index.js
@@ -0,0 +1,17 @@
+// variables
+// declare a var with const or let and (var) old
+
+let isover26 = true;
+console.log(isover26)
+
+
+// = this is what we assign to a value
+
+//==, ===, !=, !==, comparison
+// console.log("starting program");
+
+// if(isover26 && !cleanrecord) {
+//     console.log('can rent for a discount');
+// } else if{
+//     console.log('falsy')
+// }
